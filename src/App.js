137e08cbe46555bce2fa457566b9e3d3b36ab087
Jeff Eckert <bromodo@gmail.com>
2020-11-10 09:06:36 -0500

    testing backend in front end with a fetch and seems to work correctly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
+
 class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/items', {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(data => console.log(data[0].transactions))
+    
+  };
+  
+
+
   render() {
     return (
      <div className="App">
