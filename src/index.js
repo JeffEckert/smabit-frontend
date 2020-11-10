fa3e7aa73bd7fc493c45cea504a8ae5a65997b5e
Jeff Eckert <bromodo@gmail.com>
2020-11-10 14:25:43 -0500

    added redux provider and created store method,  started an enhancer method as well

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
 
 import App from './App';
 
+const enhancer = compose()
 
-ReactDOM.render(<App />,document.getElementById('root'));
+let store = createStore(reducer, applyMiddleware(thunk))
+
+
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>
+,
+document.getElementById('root'));
 
 
