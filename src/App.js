26f0d909fc50fec85911f77e1ca3338d5d5c6e40
Jeff Eckert <bromodo@gmail.com>
2020-11-10 09:56:12 -0500

    removed test fetch now that fetching from the backend is working.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,6 @@ import React from 'react';
 
 class App extends React.Component {
 
-  componentDidMount() {
-    fetch('http://localhost:3000/api/v1/items', {
-      method: 'GET'
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    
-  };
-  
 
 
   render() {
