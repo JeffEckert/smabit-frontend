3b05f5fd08cb42dc68c72ce10902a8a118586307
Jeff Eckert <bromodo@gmail.com>
2020-11-10 15:37:29 -0500

    added import connect and updated the export default in Apps.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {connect} from 'react-redux'
 
 
 class App extends React.Component {
@@ -14,4 +15,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps,{fetchItems})(App);
