923bcf6febc62fc90a14431747349fc9887065b1
Jeff Eckert <bromodo@gmail.com>
2020-11-14 15:48:46 -0500

    added a state then added value to name and balance

--- src/components/ItemInput.js
+++ src/components/ItemInput.js
@@ -2,15 +2,17 @@ import React from 'react'
 
 class ItemInput extends React.Component {
 
+state = {name: '', balance: ''}
+
     render() {
         return (
             <div>
                 <form>
                     <label>Item Name</label>
-                    <input type='text' placeholder='Name'/>
+                    <input type='text' placeholder='Name'value={this.state.name}/>
                     <br></br>
                     <label>Balance</label>
-                    <input type='text' placeholder='Balance'/>
+                    <input type='text' placeholder='Balance'value={this.state.balance}/>
                 </form>
             </div>
         )
