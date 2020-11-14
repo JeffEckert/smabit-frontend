67997106edb9209d977cca90196dc4d2dda5e703
Jeff Eckert <bromodo@gmail.com>
2020-11-14 09:38:19 -0500

    corrected typo in reducer title.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
-import accountReducer from './reducers/accountReducer'
+import accountReducer from './reducers/itemReducer'
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
