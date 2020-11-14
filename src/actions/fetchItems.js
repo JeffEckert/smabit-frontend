77bd75afe618ba533d24462abb10c76395864171
Jeff Eckert <bromodo@gmail.com>
2020-11-14 11:14:44 -0500

    removed console.logs from fetchItems file

--- src/actions/fetchItems.js
+++ src/actions/fetchItems.js
@@ -1,9 +1,7 @@
 export function fetchItems() {
     return (dispatch) => {
-      console.log('inside fetch items')
       fetch('http://localhost:3000/api/v1/items')
       .then(resp => resp.json())
-      .then(data => console.log(data))
       .then(items => dispatch({
         type: 'FETCH_ITEMS',
         payload: items
