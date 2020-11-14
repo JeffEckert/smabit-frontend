50c2bffdc35cd9428fd5553c50a3c7fe06c62c1c
Jeff Eckert <bromodo@gmail.com>
2020-11-14 15:54:01 -0500

    added onchange value for balance and name

--- src/components/ItemInput.js
+++ src/components/ItemInput.js
@@ -9,10 +9,10 @@ state = {name: '', balance: ''}
             <div>
                 <form>
                     <label>Item Name</label>
-                    <input type='text' placeholder='Name'value={this.state.name}/>
+                    <input type='text' placeholder='Name'value={this.state.name} onChange={this.handleChange}/>
                     <br></br>
                     <label>Balance</label>
-                    <input type='text' placeholder='Balance'value={this.state.balance}/>
+                    <input type='text' placeholder='Balance'value={this.state.balance} onChange={this.handleChange}/>
                 </form>
             </div>
         )
