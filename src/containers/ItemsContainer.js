85f2850c1bd5fea20bc42e9a87687a6ae7e10941
Jeff Eckert <bromodo@gmail.com>
2020-11-14 09:50:10 -0500

    created itemscontainer class in the itemsContainer folder

---
+++ src/containers/ItemsContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class ItemsContainer extends React.component {
+    
+    render() {
+        return(
+            <div>
+                ItemsContainer
+            </div>
+        )
+    }
+}
