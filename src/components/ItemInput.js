f05ee6e3ac3981bb8fb72927cfeaec05ddb510df
Jeff Eckert <bromodo@gmail.com>
2020-11-14 10:11:39 -0500

    set up component framework for ItemInput file

---
+++ src/components/ItemInput.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class ItemInput extends React.Component {
+
+    render() {
+        return (
+            <div>
+                ItemInput
+            </div>
+        )
+    }
+}
+
+export default ItemInput
