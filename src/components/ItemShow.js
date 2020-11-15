9f6954c7ed2545793f479f15badebf0442c9c38b
Jeff Eckert <bromodo@gmail.com>
2020-11-15 12:24:24 -0500

    added new item func to show a single item page with name and balance to test

--- src/components/ItemShow.js
+++ src/components/ItemShow.js
@@ -2,11 +2,14 @@ import React from 'react'
 
 
 
+
 const Item = (props) => {
+
+    let item = props.items[props.match.params.id - 1]
 
     return (
         <li>
-            {props.item.name} - {props.item.balance}
+            {item ? item.name : null} - {item ? item.balance : null}
         </li>
 
     )
