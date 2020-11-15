08186c1b47141f581bad2fe4aff50bfd8b7c56e8
Jeff Eckert <bromodo@gmail.com>
2020-11-15 11:17:22 -0500

    imported router and added router to dom

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
+import {BrowserRouter as Router} from 'react-router-dom'
 import accountReducer from './reducers/itemReducer'
 import App from './App';
 
@@ -11,7 +12,9 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 let store = createStore(accountReducer, composeEnhancers(applyMiddleware(thunk)))
 ReactDOM.render(
     <Provider store={store}>
+        <Router>
         <App />
+        </Router>
     </Provider>,
 document.getElementById('root'));
 
