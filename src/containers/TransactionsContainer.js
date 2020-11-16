79724dbdf54dcba1bab5049d6d6f71110dba2d27
Jeff Eckert <bromodo@gmail.com>
2020-11-15 19:40:38 -0500

    added a Transactions component to handle my transactions per item

--- src/containers/TransactionsContainer.js
+++ src/containers/TransactionsContainer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TransactionInput from '../components/TransactionInput'
+import Transactions from '../components/Transactions'
 
 
 class TransactionsContainer extends React.Component {
@@ -8,6 +9,7 @@ render() {
     return (
         <div>
             <TransactionInput/>
+            <Transactions/>
         </div>
     )
 
