8895c2c922eafccae35e635db3ecd9caf1c3295e
Jeff Eckert <bromodo@gmail.com>
2020-11-18 14:12:49 -0500

    added delete button to transactions and wrote handleDelete func

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -3,10 +3,14 @@ import React from 'react'
 
 const Transactions = (props) => {
 
+    handleDelete = (transaction) => {
+        this.props.deleteTransaction(transaction.id, transaction.account_id)
+      }
+
     return (
         <div>
             {props.transactions && props.transactions.map(transaction =>
-                <li key={transaction.id}>{transaction.kind} - {transaction.amount} - {transaction.note} - {transaction.date}</li>
+                <li key={transaction.id}>{transaction.kind} - {transaction.amount} - {transaction.note} - {transaction.date}<button onClick={handleDelete}>Delete</button></li>
             )}
         </div>
     )
