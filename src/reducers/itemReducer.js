2eb826e35e621a0a1acf1d5f2f85ab54a6533050
Jeff Eckert <bromodo@gmail.com>
2020-11-17 19:30:56 -0500

    added ADD_TRANSACTION to reducers

--- src/reducers/itemReducer.js
+++ src/reducers/itemReducer.js
@@ -7,6 +7,15 @@ export default function itemReducer(state = {items: []}, action) {
         return {items: action.payload}
     case 'ADD_ITEM':
         return {...state, items: [...state.items, action.payload]}
+    case 'ADD_TRANSACTION':
+            let items = state.items.map(item => {
+              if (item.id === action.payload.id) {
+                return action.payload
+              } else {
+                return item
+              }
+            })
+            return {...state, items: items}     
     default:
         return state
 
