a0b59b2d05cb4eec2fadb63d434f88212c8956b1
Jeff Eckert <bromodo@gmail.com>
2020-11-17 19:06:17 -0500

    corrected typo in state in TransactionsInput

--- src/components/TransactionInput.js
+++ src/components/TransactionInput.js
@@ -4,7 +4,7 @@ import {connect} from 'react-redux'
 class TransactionInput extends React.Component {
 
     state = {
-        kind: 'add',
+        kind: '',
         amount: '',
         note: ''
       }
