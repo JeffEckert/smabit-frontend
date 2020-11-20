132c61125772db6a5052c43fd8c2d282939ce98d
Jeff Eckert <bromodo@gmail.com>
2020-11-19 20:07:34 -0500

    fixed issue trying to get handleDelete to see item_id

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -3,20 +3,21 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {deleteTransaction} from '../actions/deleteTransaction'
 
+
 const Transactions = (props) => {
 
-  const  handleDelete = (transaction) => {
-    //   debugger;
-        props.deleteTransaction(transaction.id, transaction.item_id)
-    }
+    const handleDelete = (transaction) => {
+        debugger;
+        props.deleteTransaction(transaction.id, transaction.item_id);
+    };
+
 
     return (
         <div>
-            {props.transactions && props.transactions.map(transaction =>
-                <li key={transaction.id}>{transaction.kind} - {transaction.amount} - {transaction.note} - {transaction.date}<button onClick={() => handleDelete(transaction)}>Delete</button></li>
+            {props.transactions && props.transactions.map(transaction => <li key={transaction.id}>{transaction.kind} - {transaction.amount} - {transaction.note} - {transaction.date} <button onClick={() => handleDelete(transaction)}>Delete</button></li>
             )}
         </div>
-    )
+    );
 
 }
 
