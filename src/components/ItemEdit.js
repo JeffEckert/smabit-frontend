98cb733432e3658458f18fa60646c413e6d1fb42
Jeff Eckert <bromodo@gmail.com>
2020-11-20 21:22:40 -0500

    added an editItem action

--- src/components/ItemEdit.js
+++ src/components/ItemEdit.js
@@ -27,25 +27,19 @@ class ItemEdit extends React.Component {
       }
 
 
-    // render() {
-    //     return (
-    //       <div>
-    //         <form onSubmit={this.handleSubmit}>
-    //           <label>Checking Item Name: </label>
-    //           <input type='text' placeholder='Name' value={this.state.name} name="name" onChange={this.handleChange}/><br/>
-    //           <label>Checking item Balance: </label>
-    //           <input type='text' placeholder='Balance' value={this.state.balance} name="balance" onChange={this.handleChange}/><br/>
-    //           <input type="submit"/>
-    //         </form>
-    //       </div>
-    //     )
-    //   }
-    // }
-
-
-
-
-
+    render() {
+        return (
+          <div>
+            <form onSubmit={this.handleSubmit}>
+              <label>Checking Item Name: </label>
+              <input type='text' placeholder='Name' value={this.state.name} name="name" onChange={this.handleChange}/><br/>
+              <label>Checking item Balance: </label>
+              <input type='text' placeholder='Balance' value={this.state.balance} name="balance" onChange={this.handleChange}/><br/>
+              <input type="submit"/>
+            </form>
+          </div>
+        )
+    }
 }
 
 export default ItemEdit
