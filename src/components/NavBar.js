425abe773e802ae41a607d2c67ec711193fd580a
Jeff Eckert <bromodo@gmail.com>
2020-11-21 16:13:35 -0500

    added NavBar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import Navbar from 'react-bootstrap/NavBar'
+import Nav from 'react-bootstrap/Nav'
 
 const NavBar = (props) => {
 
   return (
     <div>
-      <Link to='/items' style={{paddingRight: '10px'}}>Items  </Link>
-      <Link to='/items/new'style={{paddingRight: '10px'}}> Add Item  </Link>
-      <Link to='/'>  Home</Link>
+        <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">SmaBIT</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Link to='/items' style={{paddingRight: '10px'}}>Items  </Link>
+                <Link to='/items/new'style={{paddingRight: '10px'}}> Add Item  </Link>
+                <Link to='/'>Home</Link>
+            </Nav>
+
+        </Navbar>
+
     </div>
 
   )
