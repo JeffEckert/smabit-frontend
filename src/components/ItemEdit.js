7d8bb5f5687f98a5bb89ad2fcef02e9f7f30c9ac
Jeff Eckert <bromodo@gmail.com>
2020-11-20 21:17:32 -0500

    added state to ItemEdit component.

--- src/components/ItemEdit.js
+++ src/components/ItemEdit.js
@@ -5,21 +5,26 @@ class ItemEdit extends React.Component {
 
      
 
-
-    render() {
-        return (
-          <div>
-            <form onSubmit={this.handleSubmit}>
-              <label>Checking Item Name: </label>
-              <input type='text' placeholder='Name' value={this.state.name} name="name" onChange={this.handleChange}/><br/>
-              <label>Checking Account Balance: </label>
-              <input type='text' placeholder='Balance' value={this.state.balance} name="balance" onChange={this.handleChange}/><br/>
-              <input type="submit"/>
-            </form>
-          </div>
-        )
-      }
+    state = {
+        name: '',
+        balance: ''
     }
+
+
+    // render() {
+    //     return (
+    //       <div>
+    //         <form onSubmit={this.handleSubmit}>
+    //           <label>Checking Item Name: </label>
+    //           <input type='text' placeholder='Name' value={this.state.name} name="name" onChange={this.handleChange}/><br/>
+    //           <label>Checking Account Balance: </label>
+    //           <input type='text' placeholder='Balance' value={this.state.balance} name="balance" onChange={this.handleChange}/><br/>
+    //           <input type="submit"/>
+    //         </form>
+    //       </div>
+    //     )
+    //   }
+    // }
 
 
 
