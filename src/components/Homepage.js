32aec68015f3c56e30250d66deb0a184dd2a58d5
Jeff Eckert <bromodo@gmail.com>
2020-11-21 20:04:03 -0500

    added some basic styling to homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,19 @@
 import React from 'react'
-
-// import jumbotron from 'react-bootstrap/jumbotron'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Button from 'react-bootstrap/Button'
 
 const Homepage = () => {
 
         return(
-            <h2>Test</h2>
+
+            <Jumbotron>
+            
+            <h2>Welcome to SmaBIT</h2>
+            <h3>Your complete Small Business Inventory Tracker</h3>
+            <p>To get started please click on the button below to Create your first Item to track.</p>
+            <Button variant="success">Create Item</Button>
+
+            </Jumbotron>
         )
 }
 
